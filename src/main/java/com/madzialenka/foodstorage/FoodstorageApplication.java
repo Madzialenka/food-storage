0e84a8a64cc611fc85712b698fe5b2a0ed8fdb7b
Magdalena Ceglińska <m.ceglinska1999@gmail.com>
2020-12-12 11:47:25 +0100

    basic structure of application & db configuration

---
+++ src/main/java/com/madzialenka/foodstorage/FoodstorageApplication.java
@@ -0,0 +1,13 @@
+package com.madzialenka.foodstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodstorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodstorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/madzialenka/foodstorage/FoodstorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madzialenka.foodstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodstorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
